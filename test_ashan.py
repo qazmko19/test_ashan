0933e545195b2641f62120a35bf218f049c9b308
qazmko19 <nikitatslnk@gmail.com>
2024-03-07 13:31:36 +0200

    final commit 07.03 13:31

--- test_ashan.py
+++ test_ashan.py
@@ -1,83 +1,121 @@
-# Імпорт необхідних бібліотек/модулів
+# Импорт необходимых модулей/библиотек
+import os
 import pickle
+import time
+from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime, timedelta
 from urllib.parse import urlparse
+import requests.exceptions
+from loguru import logger
 from urlextract import URLExtract
-import requests
+
+# Начало работы программы
+start_time = time.time()
+
+# Настройки логирования: директория, название файла, частота создания
+logger.add('logs/{time:YYYY-MM-DD_HH-mm-ss}.log', rotation='5 minutes')
+LOGS_DIR = "logs/"
+
+# Создать директорию если её нет
+os.makedirs(LOGS_DIR, exist_ok=True)
+
+# Десериализация файла
+with open("messages_to_parse.dat", 'rb') as f:
+    data_list = pickle.load(f)
+
+# Приведение данных к типу данных str
+data = ' '.join(data_list)
+
+# Создание объекта класса URLExtract
+extractor = URLExtract()
+
+# Поиск всех URL адресов
+urls = extractor.find_urls(data)
+
+# Создание необходимых словарей
+status_codes = {}
+final_urls = {}
 
 
-def deserialization(file):
-    with open(file, 'rb') as f:
-        data_list = pickle.load(f)
-
-    return ' '.join(data_list)
-
-
-def url_extract(data):
-    extractor = URLExtract()
-
-    return extractor.find_urls(data)
+# Функция удаления файлов логирования старше 20 минут
+def delete_old_logs():
+    now = datetime.now()
+    for log_file in os.listdir(LOGS_DIR):
+        log_file_path = os.path.join(LOGS_DIR, log_file)
+        creation_time = datetime.fromtimestamp(os.path.getctime(log_file_path))
+        if now - creation_time > timedelta(minutes=20):
+            os.remove(log_file_path)
+            logger.info(f"Deleted old log file: {log_file_path}")
 
 
-def fill_status_codes(urls_data):
-    status_codes = {}
-    for i in range(len(urls_data)):
-        url = urls_data[i]
-
-        try:
-            response = requests.head(url, timeout=5)
-        except requests.exceptions.MissingSchema:
-            response = requests.head('https://' + urls_data[i])
-        except requests.exceptions.RequestException:
-            status_codes[url] = -1
-            continue
-
-        status_code = response.status_code
-        status_codes[url] = status_code
-
-    return status_codes
+# Выполнение функции удаления старых файлов логирования
+delete_old_logs()
+logger.info("Started deleting old log files")
+logger.info("Starting processing URLs...")
 
 
-def status_dictionary(status_codes):
-    for url, status_code in status_codes.items():
-        print(f"URL: {url}, Status code: {status_code}")
+# Функция заполнения словаря с ключами - URL и значениями - Status code
+def fill_status_codes(fsc_url):
+    try:
+        response = requests.head(fsc_url, timeout=5)
+    except requests.exceptions.MissingSchema:
+        response = requests.head('https://' + fsc_url, timeout=5)
+        logger.warning(f"{fsc_url} is having MissingSchema exception. Request send to {'https://' + fsc_url}")
+    except requests.exceptions.RequestException:
+        logger.error(f"{fsc_url} is unreachable. Status code of this URL is -1")
+        status_codes[fsc_url] = -1
+        pass
+
+    logger.info(f"URL: {fsc_url}; Status code: {response.status_code}")
+    status_codes[fsc_url] = response.status_code
 
 
-def fill_final_urls(urls_data):
-    final_urls = {}
-    for i in range(len(urls_data)):
-        url = urls_data[i]
+# Функция заполнения словаря с ключами - оригинальный URL и значениями - финальный URL
+def fill_final_urls(ffu_url):
+    try:
+        response = requests.head(ffu_url, allow_redirects=True, timeout=5)
+    except requests.exceptions.MissingSchema:
+        response = requests.head('https://' + ffu_url, allow_redirects=True, timeout=5)
+        logger.warning(f"{ffu_url} is having MissingSchema exception. Request send to {'https://' + ffu_url}")
+    except requests.exceptions.RequestException:
+        logger.error(f"{ffu_url} is unreachable. Unable to check short URL")
+        pass
 
-        try:
-            response = requests.head(url, allow_redirects=True, timeout=5)
-        except requests.exceptions.MissingSchema:
-            response = requests.head('https://' + urls_data[i])
-        except requests.exceptions.RequestException:
-            continue
+    parsed_original = urlparse(ffu_url)
+    parsed_final = urlparse(response.url)
 
-        final_url = response.url
-        parsed_original = urlparse(url)
-        parsed_final = urlparse(final_url)
-
-        if is_short_url(parsed_original, parsed_final):
-            final_urls[url] = final_url
-
-    return final_urls
+    if parsed_original.netloc != parsed_final.netloc:
+        logger.info(f"URL: {ffu_url}; Final URL: {response.url}")
+        final_urls[ffu_url] = response.url
 
 
-def final_urls_dictionary(final_urls):
-    for url, final_url in final_urls.items():
-        print(f"URL: {url}, Final URL: {final_url}")
+# Использование потоков для ускорения работы функции fill_status_codes()
+with ThreadPoolExecutor(max_workers=8) as executor:
+    executor.map(fill_status_codes, urls)
 
+# Использование потоков для ускорения работы функции fill_final_urls()
+with ThreadPoolExecutor(max_workers=8) as executor:
+    executor.map(fill_final_urls, urls)
 
-def is_short_url(original, final):
-    return original.netloc != final.netloc
+# Конец отсчёта времени работы программы
+end_time = time.time()
 
+# for url, status_code in status_codes.items():
+#     print(f"URL: {url}; Status code: {status_code}")
+#
+# for url, final_url in final_urls.items():
+#     print(f"URL: {url}; Final URL: {final_url}")
 
-if __name__ == "__main__":
-    data = deserialization("messages_to_parse.dat")
-    urls = url_extract(data)
-    status_codes = fill_status_codes(urls)
-    status_dictionary(status_codes)
-    final_urls = fill_final_urls(urls)
-    final_urls_dictionary(final_urls)
+# Разделение времени работы программы на минуты, секунды и миллисекунды
+result_time = end_time - start_time
 
+minutes = int(result_time // 60)
+seconds = int(result_time % 60)
+milliseconds = int((result_time - seconds) * 1000)
+
+# Информация о времени работы программы
+logger.info(f"Finished processing URLs. Time is {minutes:02d}:{seconds:02d}:{milliseconds:02d}")
+
+# Удаление старых файлов логирования
+delete_old_logs()
+logger.info("Finished deleting old log files")
